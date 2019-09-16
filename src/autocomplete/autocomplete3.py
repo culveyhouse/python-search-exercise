9750cda2053d5a09093ccd2f339fc023caa14c8f
Ubuntu <ubuntu@ip-172-31-35-102.us-east-2.compute.internal>
2019-09-16 08:38:51 +0000

    Finished Autocomplete3 class, verified with pytest criteria
    1. Added additional arg "limit", since this will be used for web autocomplete
    and may need a limit.
    2. Used regular expressions with re.compile and re.search for efficiency and
    standardization.
    3. Used a simple lambda/sorted pair to sort results before returning
    3. Employed UTF-8 encoding in the event that suggestion data contains UTF-8
    characters.

--- src/autocomplete/autocomplete3.py
+++ src/autocomplete/autocomplete3.py
@@ -1,8 +1,10 @@
+import re
+
 class Autocomplete3:
     def __init__(self, suggestions):
         self.suggestions = suggestions
 
-    def performSearch(self, term):
+    def performSearch(self, term, limit=None):
         """Searches through the possible suggestions, creating an array of
         matches for any books whose title matches the specified search term.
 
@@ -15,11 +17,29 @@ class Autocomplete3:
             should also be returned.
 
         Args:
-            term - the term to search for in book titles.
+            term = the term to search for in book titles.
+            limit = maximum number of autocomplete suggestions to return 
 
         Return:
-            List of results where the title contains the search term. Results
-            should be sorted alphabetically by title.
+            List of results (dictionary entries) where the title contains 
+            the search term.
         """
-        # TODO: Implement this.
-        return []
+        
+        # A list of dictionary entries that match the "term" argument
+        results = [] 
+
+        # Compiled case-insensitive regex search using "term" argument for efficiency
+        term_search = re.compile(r"{}".format(term), re.I) 
+
+        for suggestion in self.suggestions:
+            # Our suggestions may contain special UTF-8 characters
+            title = suggestion['title'].encode("utf-8")
+            if term_search.search(title): 
+                results.append(suggestion)
+                
+                # Limit autocomplete results if limit passed
+                if limit and len(results)>=limit: 
+                    break
+    
+        sorted_results = sorted(results, key = lambda suggestion: suggestion['title'])
+        return sorted_results
