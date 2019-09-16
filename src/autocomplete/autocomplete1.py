3f4520c8199449bdf00b9e78132751edae309afd
Ubuntu <ubuntu@ip-172-31-35-102.us-east-2.compute.internal>
2019-09-16 08:29:59 +0000

    Finished Autocomplete1 class, verified with pytest criteria
    1. Added additional arg "limit", since this will be used for web autocomplete
    and may need a limit.
    2. Used regular expressions with re.compile and re.search for efficiency and
    standardization.
    3. Employed UTF-8 encoding in the event that suggestion data contains UTF-8
    characters.

--- src/autocomplete/autocomplete1.py
+++ src/autocomplete/autocomplete1.py
@@ -1,8 +1,10 @@
+import re
+
 class Autocomplete1:
     def __init__(self, suggestions):
         self.suggestions = suggestions
 
-    def performSearch(self, term):
+    def performSearch(self, term, limit=None):
         """Searches through the possible suggestions, creating an array of
         matches for any books whose title matches the specified search term.
 
@@ -14,10 +16,28 @@ class Autocomplete1:
             should not be returned.
 
         Args:
-            term - the term to search for in book titles.
+            term = the term to search for in book titles.
+            limit = maximum number of autocomplete suggestions to return 
 
         Return:
-            List of results where the title contains the search term.
+            List of results (dictionary entries) where the title contains 
+            the search term.
         """
-        # TODO: Implement this.
-        return []
+        
+        # A list of dictionary entries that match the "term" argument
+        results = [] 
+
+        # Compiled regex search using "term" argument for efficiency
+        term_search = re.compile(r"{}".format(term)) 
+
+        for suggestion in self.suggestions:
+            # Our suggestions may contain special UTF-8 characters            
+            title = suggestion['title'].encode("utf-8")
+            if term_search.search(title): 
+                results.append(suggestion)
+                
+                # Limit autocomplete results if limit passed
+                if limit and len(results)>=limit: 
+                    break
+
+        return results
