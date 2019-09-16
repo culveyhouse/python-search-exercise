7e5b7b27a2af5745a93d95e30db4b40e3a5d9cd6
Ubuntu <ubuntu@ip-172-31-35-102.us-east-2.compute.internal>
2019-09-16 08:36:21 +0000

    Finished Autocomplete2 class, verified with pytest criteria
    1. Added additional arg "limit", since this will be used for web autocomplete
    and may need a limit.
    2. Used regular expressions with re.compile and re.match for efficiency and
    standardization.
    3. Employed UTF-8 encoding in the event that suggestion data contains UTF-8
    characters.

--- src/autocomplete/autocomplete2.py
+++ src/autocomplete/autocomplete2.py
@@ -1,8 +1,10 @@
+import re
+
 class Autocomplete2:
     def __init__(self, suggestions):
         self.suggestions = suggestions
 
-    def performSearch(self, term):
+    def performSearch(self, term, limit=None):
         """Searches through the possible suggestions, creating an array of
         matches for any books whose title matches the specified search term.
 
@@ -15,9 +17,27 @@ class Autocomplete2:
 
         Args:
             term - the term to search for in book titles.
+            limit = maximum number of autocomplete suggestions to return            
 
         Return:
-            List of results where the title starts with the search term.
+            List of results (dictionary entries) where the title contains 
+            the search term.
         """
-        # TODO: Implement this.
-        return []
+        
+        # A list of dictionary entries that match the "term" argument
+        results = [] 
+
+        # Compiled regex search using "term" argument for efficiency
+        prefix_search = re.compile(r"^{}".format(term)) 
+
+        for suggestion in self.suggestions:
+            # Our suggestions may contain special UTF-8 characters
+            title = suggestion['title'].encode("utf-8")            
+            if prefix_search.match(title): 
+                results.append(suggestion)
+                
+                # Limit autocomplete results if limit passed
+                if limit and len(results)>=limit: 
+                    break
+
+        return results
